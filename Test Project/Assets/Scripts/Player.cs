b26045d2827945d107a69feacda21ee48e3ca6e5
avebryhv <33032139+avebryhv@users.noreply.github.com>
2018-10-08 12:41:15 +0100

    8/10

---
+++ Test Project/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    private float movementSpeed = 10;
+    public float turningSpeed = 60;
+    public float thrust;
+    public float charge;
+    public Rigidbody rb;
+    public Slider boostSlider;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        //float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        //transform.Translate(0, 0, vertical);
+
+        if (Input.GetKey("e"))
+        {
+            charge += 1;
+            //rb.AddForce(transform.forward * thrust);
+        }
+        if (Input.GetKeyUp("e"))
+        {
+            rb.AddForce(transform.forward * charge * thrust);
+            charge = 0;
+        }
+        boostSlider.value = charge;
+
+    }
+}
