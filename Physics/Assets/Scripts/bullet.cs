c85e3ee2e6fc7df30d162e5a3a9e1cae7cb7e8f3
avebryhv <33032139+avebryhv@users.noreply.github.com>
2018-10-09 11:57:28 +0100

    9/10

---
+++ Physics/Assets/Scripts/bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 60f, ForceMode.Impulse);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
